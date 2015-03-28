cbbea35cbc8f0de396207ea06cba07d3a95fed4e
Konrad Zdeb <konrad.zdeb@me.com>
2015-03-28 10:16:27 +0000

    Addittional comments

--- server.R
+++ server.R
@@ -2,26 +2,27 @@
 library(shiny); require(ggplot2); require(grid)
 
 shinyServer(function(input, output) {
-  
+
   # Run the data download from NOMIS and run other data cleaning
   source('global.R', local=TRUE)
-  
+
   # Render chart
   output$main_plot <- renderPlot({
     # Filter the data set accordingly
-    dta.chrt <- subset(x = dta.nom, 
+    dta.chrt <- subset(x = dta.nom,
                        subset = dta.nom$GEOGRAPHY_NAME %in% input$checkCouncil &
                          dta.nom$MEASURES_NAME %in% input$selectMeasure &
                          dta.nom$ITEM_NAME %in% input$selectIndicator &
                          dta.nom$DATE_NAME >= input$sliderYears[1] &
                          dta.nom$DATE_NAME <= input$sliderYears[2])
     # Define legend for convenience and alter the row number if required
+    ## Legend rows are adjusted according to geographies
     g <- guide_legend(title = "Geography", title.position = 'top',
                       nrow = (if(length(unique(dta.chrt$GEOGRAPHY_NAME)) > 5) 2
                         else 1))
     # Graph something
     ggplot(data = dta.chrt, aes(x = DATE_NAME, y = OBS_VALUE)) +
-      geom_line(aes(linetype = GEOGRAPHY_NAME, colour = GEOGRAPHY_NAME), 
+      geom_line(aes(linetype = GEOGRAPHY_NAME, colour = GEOGRAPHY_NAME),
                 size = input$sliderLine) +
       geom_point(aes(shape = GEOGRAPHY_NAME, colour = GEOGRAPHY_NAME),
                  size = input$sliderPoint) +
@@ -30,15 +31,15 @@ shinyServer(function(input, output) {
       ylab(input$selectIndicator) +
       guides(colour = g, shape = g, linetype = g) +
       theme(panel.background = element_rect(fill = NA),
-            panel.grid.major = element_line(colour = 'gray', 
+            panel.grid.major = element_line(colour = 'gray',
                                             linetype = 'dotted'),
             legend.position = 'bottom',
             legend.background = element_rect(colour = 'black',
-                                             fill = NA, size = 0.5, 
+                                             fill = NA, size = 0.5,
                                              linetype = 'solid'),
             legend.key.width = unit(2, "cm"),
             legend.key = element_blank(),
-            axis.title = element_text(colour = 'black', face = 'bold', 
+            axis.title = element_text(colour = 'black', face = 'bold',
                                       size = 12),
             axis.text = element_text(colour = 'black', size = 12),
             plot.background = element_rect(colour = 'gray'),
@@ -49,7 +50,7 @@ shinyServer(function(input, output) {
     # Filter the data set accordingly
     dta.chrt <- subset(x = dta.nom[,c("GEOGRAPHY_NAME",
                                       "MEASURES_NAME","ITEM_NAME",
-                                      "DATE_NAME")], 
+                                      "DATE_NAME")],
                        subset = dta.nom$GEOGRAPHY_NAME %in% input$checkCouncil &
                          dta.nom$MEASURES_NAME %in% input$selectMeasure &
                          dta.nom$ITEM_NAME %in% input$selectIndicator &
